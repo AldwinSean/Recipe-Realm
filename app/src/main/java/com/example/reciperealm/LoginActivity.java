18a8f4b4e8a34c997e9a168ac393841b5ccc3526
AldwinSean <164906584+AldwinSean@users.noreply.github.com>
2024-03-28 13:44:54 -0600

    Login and Signup changes

--- app/src/main/java/com/example/reciperealm/LoginActivity.java
+++ app/src/main/java/com/example/reciperealm/LoginActivity.java
@@ -2,13 +2,24 @@ package com.example.reciperealm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+//        requestWindowFeature(Window.FEATURE_NO_TITLE);
+//        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login);
+    }
+
+    public void signupdirectory(View view) {
+        startActivity(new Intent(LoginActivity.this, SignUpActivity.class));
     }
 }
